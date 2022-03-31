ea129ccde43f807038b15b58f2ac098027af1778
Melonps <Melonpastap@gmail.com>
2022-03-31 20:15:18 +0900

    [ADD] 部員募集の実装
    これでペンタスに見せます

--- src/views/Advertise.vue
+++ src/views/Advertise.vue
@@ -1,5 +1,245 @@
 <template>
   <div class="advertise">
-    <h1>部員募集</h1>
+    <div class="image_header"><img src="../assets/header_Ad.png"></div>
+    <h1 class="siteTt_1">ようこそ公立大吹奏楽部へ！</h1>
+    <div class="contents_above">
+      <div class="txt_1">
+        <p>大阪公立大学吹奏楽部では年間を通じて部員を募集しています。<br>
+        まずは活動紹介をご覧ください。活動の用紙や部の雰囲気を紹介しております。
+        </p>
+      </div>
+      <div class="box">
+        <p class="txt_in_box">活動紹介</p>
+      </div>
+      <div class="txt_2">
+        <p>入部希望者や、活動に興味がある方は募集要項を確認の上、<br>
+        募集要項記載の連絡先にお問い合わせください。<br>
+        直接活動場所に出向いていただいても構いません。<br>
+        話を聞くだけや部の雰囲気を見たいだけの方でも大歓迎です！<br>
+        </p>
+      </div>
+    </div>
+    <h1 class="siteTt_1">募集要項</h1>
+    <div class="contents_below">
+      <table class="table_Ad" >
+        <tr>
+          <td width="30%">活動場所</td>
+          <td width="70%">大阪公立大学中百舌鳥キャンパス<br>
+            練習場所:B12棟 学生会館2階 大集会室 小集会室など<br>
+            部室:文化部棟2階<br>
+          </td>
+        </tr>
+        <tr>
+          <td>活動日時</td>
+          <td>毎週火・金・土曜日<br><br>
+          授業日：16:30~19:00<br>
+          休講日：13:30~18:00
+          </td>
+        </tr>
+        <tr>
+          <td>部費</td>
+          <td>楽器自持ち:￥1500/月<br>
+              楽器貸し出し:￥2000/月<br>
+              ただし、演奏会後のオフ期間や、感染拡大による休部期間は徴収しません<br>
+          </td>
+        </tr>
+        <tr>
+          <td>募集対象者</td>
+          <td>大学生(公立大生だけでなく他大生も歓迎)<br>
+          楽器経験不問<br>
+          </td>
+        </tr>
+        <tr>
+          <td>他大生入部実績</td>
+          <td>大阪工業大学、関西大学、近畿大学、帝塚山学院大学、同支社大学、武庫川女子大学、森ノ宮医療大学</td>
+        </tr>
+        <tr>
+          <td>入部フロー</td>
+          <td>下記連絡先に「〇〇大学の〇〇(名前)です！見学希望です！」などの内容でDM→当部からの返信にて日程調整→部活動見学→楽器希望提出(仮入部)→担当楽器確定(入部)</td>
+        </tr>
+        <tr>
+          <td>連絡先</td>
+          <td>メール:<a href="mailto:opubrass.sns@gmail.com">opubrass.sns@gmail.com</a><br>
+          Twitter:<a href="https://twitter.com/omu_wind">@omu_wind</a><br>
+          Instagram:<a href="https://www.instagram.com/omuwind/?hl=ja">omu_wind</a><br>
+          ※TwitterまたはInstagramのDMがおすすめです
+          </td>
+        </tr>
+      </table>
+      
+    </div>
+  </div>
+  <h1 class="siteTt_1">Q&A</h1>
+  <div class="contents_QA">
+    <div class="txt_3">活動に関する質問は質問箱にて受付してます。<br>些細な質問でも気軽にどうぞ！
+    </div>
+    <table class="table_qa">
+      <tr>
+        <td>
+          <div class="txt_Q">
+            <div class="icon_Q">他大生で、今学期の履修では平日は練習時間に間に合いません..<br>
+              さすがに土曜だけ参加とかはダメですよね...</div>
+          </div>
+          <div class="txt_A">
+            <div class="icon_A">大丈夫です！公立大吹奏楽部では個人の生活、学業を優先しているので週１での参加も可能です！<br>
+            過去の先輩にも〇回生の頃は〇曜日だけ来てた！って人は多いです！</div>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="txt_Q">
+            <div class="icon_Q">りんくうキャンパスから通うのって難しいですかね…?</div>
+          </div>
+          <div class="txt_A">
+            <div class="icon_A">大丈夫です！他キャンパスから通っている部員も多く、これる日にうまく来て部活を楽しんでいます！<br>
+            いろんなキャンパスの部員がいるので授業の情報をゲットできるかも..?!</div>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <Td>
+          <div class="txt_Q">
+            <div class="icon_Q">空きは練習などができますか？部室で過ごすことも可能なのでしょうか？</div>
+          </div>
+          <div class="txt_A">
+            <div class="icon_A">空きコマは部室と練習場所を解放しています！自由に練習したり、ごはんを食べたりできます！</div>
+          </div>
+        </Td>
+      </tr>
+      <tr>
+        <td>
+          <div class="txt_Q">
+            <div class="icon_Q">本当に初心者でも大丈夫なんですか？？</div>
+          </div>
+          <div class="txt_A">
+            <div class="icon_A">大阪公立大学吹奏楽部は、初心者でも楽しい部活だと思います！<br>
+            吹部には、演奏以外にも演奏会の運営や企画なども盛りだくさんですし、大学生ならではの効率のいい練習法でガンガン吹けるようになります！！</div>
+          </div>
+        </td>
+      </tr>
+    </table>
+  
   </div>
 </template>
+
+<style scoped>
+h1.siteTt_1{
+  margin-top: 5%;
+  font-size: 2rem;
+}
+table.table_Ad {
+  text-align: left;
+  width: 100%;
+}
+table.table_Ad td{
+  margin: auto;
+  border-width: 1px 0px;
+  padding:1rem 4rem;
+}
+
+table.table_qa{
+  width: 100%;
+  margin: auto;
+}
+table.table_qa td{
+  margin: auto;
+}
+table.table_qa tr{
+  border-bottom: solid 1px gray;
+}
+
+p.txt_in_box{
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+.box {
+  width: 40%;
+  margin: 1rem auto;
+  border: 1px solid #333;
+  padding: 1rem 0;
+}
+.image_header img {
+	display: block;
+	width: 100%;
+	height: auto;
+  object-fit: cover;
+}
+.before{
+  padding: 10px;
+  border: 1px solid #333;
+  text-align: center;
+}
+
+.icon_Q::before{
+  content: "";
+  display: inline-block;
+  background: url("../assets/icon_Q.png") no-repeat 0 0 / 1.4em auto;
+  width: 1.4em;
+  height: 1.4em;
+  margin-right: 0.1em;
+  vertical-align: middle;
+}
+.icon_A::before{
+  content: "";
+  display: inline-block;
+  background: url("../assets/icon_A.png") no-repeat 0 0 / 1.4em auto;
+  width: 1.4em;
+  height: 1.4em;
+  margin-right: 0.1em;
+  vertical-align: middle;
+}
+.txt_Q{
+  text-align: left;
+  margin-left: 1.4em;
+	text-indent: -1.4em;
+  margin-bottom: 1.5em;
+  font-weight: bold;
+  margin-top: 1em;
+}
+.txt_A{
+  text-align: left;
+  margin-left: 1.5em;
+	text-indent: -1.5em;
+  margin-bottom: 1.5em;
+}
+.txt_3{
+  display: inline-block;
+  text-align: left;
+	text-indent: 0;
+  background: url("../assets/icon_QA.png") no-repeat left center/ 4em auto;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  padding-left: 4.5em;
+}
+.contents_above {
+  text-align: center;
+  width: 83%;
+  max-width: 800px;
+  margin: 5% auto;
+  font-family:
+	'Yu Mincho', /* Windows用 */
+	serif;
+  font-size: 1rem;
+}
+.contents_below {
+  width: 80%;
+  max-width: 1200px;
+  margin: 5% auto;
+  font-family :
+	'Yu Mincho', /* Windows用 */
+	serif;
+  font-size: 1rem;
+}
+.contents_QA {
+  width: 80%;
+  max-width: 1000px;
+  margin: 3% auto;
+  font-family :
+	'Yu Mincho', /* Windows用 */
+	serif;
+  position: relative;
+  font-size: 1rem;
+}
+
+</style>
