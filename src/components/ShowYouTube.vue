0934e6b0b1aeaa3e3e85900e2c50c2f263cb7f71
Yukairon09 <yukarionn00@gmail.com>
2022-04-04 18:57:15 +0900

    [ADD] ShowYouTubeの実装とFooterの修正 #27
    実装しましたが，いい感じの実装はできなくて結局APIキー直書きとかいう手法に頼りました．時間ができたら修正したい
    できなかった理由はvue-cliでphpを含んでコンパイルする方法がわからない(バカ？)こととasync/awaitがいまいち理解できてない(学習してる時間もあまりなさそう．でもasync createdは使えそうなので未来の自分には頑張ってほしい)とサーバがどうなってるのかわかってないことあたり．力と時間がほしい

--- src/components/ShowYouTube.vue
+++ src/components/ShowYouTube.vue
@@ -1,5 +1,12 @@
 <template>
-
+  <div class="youtube">
+    <div class v-if="load">
+      <iframe class="embed-responsive-item" :src="link" title="YouTube video player" allow=" fullscreen"></iframe>
+    </div>
+    <div v-else>
+      Now loading...
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,8 +14,30 @@ export default {
   name: "ShowYouTube",
   data() {
     return {
+      load: false,
+      link: "https://www.youtube.com/embed/"
     }
   },
+  created(){
+    const apikey = 'AIzaSyADlM2uDjO7Lm0Nq9brtdhzEeGtoG1vNko';
+    const playlistid = 'UUvrAz5r2EVMbWNOEDoH0k8A';
+    const maxresults = '1';
+    const url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults='+maxresults +'&playlistId='+playlistid+'&key='+apikey;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      var json = JSON.parse(xhr.responseText || null);
+      console.log(json);
+      console.log(this.link);
+      var videoid = "";
+      videoid = json.items[0].snippet.resourceId.videoId;
+      this.link = this.link + videoid;
+      console.log(this.link);
+      this.load = true;
+    };
+  },
+};
 </script>
 
 <style>
--- src/components/Topics.vue
+++ src/components/Topics.vue
@@ -1,13 +1,12 @@
 <template>
-<ShowInstagram />
-<ShowTwitter />
-<ShowYouTube />
-
+  <!-- <ShowInstagram /> -->
+  <!-- <ShowTwitter /> -->
+  <ShowYouTube />
 </template>
 
 <script>
-import ShowInstagram from "@/components/ShowInstagram.vue";
-import ShowTwitter from "@/components/ShowTwitter.vue";
+// import ShowInstagram from "@/components/ShowInstagram.vue";
+// import ShowTwitter from "@/components/ShowTwitter.vue";
 import ShowYouTube from "@/components/ShowYouTube.vue";
 export default {
   data() {
@@ -17,8 +16,9 @@ export default {
   methods: {
   },
   components: {
-    Header,
-    Footer,
+    ShowYouTube,
+//    ShowTwitter,
+//    ShowInstagram,
   },
 };
 </script>
