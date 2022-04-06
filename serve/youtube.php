01196f0f74f96e7f3844dc40b488fd5d4d64b479
Yukairon09 <yukarionn00@gmail.com>
2022-04-06 16:06:31 +0900

    [FIX] バックエンドを追加 #27
    コピペコード 多分動くので採用
    あとでちゃんと色んな場所を書き直す

---
+++ serve/youtube.php
@@ -0,0 +1,41 @@
+<?php
+//Google APIキー
+$apikey = 'AIzaSyADlM2uDjO7Lm0Nq9brtdhzEeGtoG1vNko';
+//Youtube プレイストID
+$playlistid = 'UUvrAz5r2EVMbWNOEDoH0k8A';
+//取得件数
+$maxresults = '4';
+//API URL
+$url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults='.$maxresults .'&playlistId='.$playlistid.'&key='.$apikey;
+//ローカルjsonファイル名
+$filename = 'youtube.json';
+
+//データ更新
+function update($filename,$url){
+	$json = @file_get_contents($filename);
+	$handle = fopen($filename, 'w');
+	if(flock($handle, LOCK_EX)){
+		if($data = @file_get_contents($url)){
+			$json = $data;
+		}
+		fwrite($handle, $json);
+	}
+	fclose($handle);
+}
+
+//ローカルjson更新時間チェック
+if(file_exists($filename)){
+	$updatetime = filemtime($filename);
+	$diff = time() - $updatetime;
+	if($diff >= 600) {
+		update($filename,$url);
+	}
+}else{
+	update($filename,$url);
+}
+
+//ローカルjson出力
+$json = @file_get_contents($filename);
+header('Content-Type: application/json');
+echo $json;
+?>
