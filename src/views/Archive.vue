14d7217093ecba30191087b24474756d91c73f41
Melonps <Melonpastap@gmail.com>
2022-04-07 19:42:03 +0900

    [ADD] 開発中ですごめんなさいの追加 #37
    しました。

--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="archive">
+  <div class="request">
     <h1>過去の演奏会</h1>
+    <h2>開発中ですごめんなさい</h2>
+    <p>間に合いませんでした。普通に。</p>
+    <div class="tmp"><img src="../assets/now_dev.png"></div>
   </div>
 </template>
+
+<style>
+  .tmp {
+    display: block;
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+</style>
--- src/views/Link.vue
+++ src/views/Link.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="link">
+  <div class="request">
     <h1>リンク</h1>
+    <h2>開発中ですごめんなさい</h2>
+    <p>間に合いませんでした。普通に。</p>
+    <div class="tmp"><img src="../assets/now_dev.png"></div>
   </div>
 </template>
+
+<style>
+  .tmp {
+    display: block;
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+</style>
