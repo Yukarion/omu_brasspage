5382e73ed05edb01de882779b507eae09fe71877
Melops <95263124+Melonps@users.noreply.github.com>
2024-01-20 00:50:16 +0900

    [FIX] Greetingのいらないクラスを削除

--- src/views/Greeting.vue
+++ src/views/Greeting.vue
@@ -1,23 +1,19 @@
 <template>
-    <div class="greeting">
-        <HeroImage fileName="greeting_header.png" title="ごあいさつ" />
-        <div class="large-wrapper">
-        <div class="wrapper">
-            <div class="title">
-                <h1>Osaka Metropolitan University Wind Orchestra</h1>
-            </div>
-            <div class="sub_title">
-                <h2>大阪公立大学吹奏楽部</h2>
-            </div>
-            <p>
-                本日も大阪公立大学吹奏楽部のホームページをご覧いただき、誠にありがとうございます。
-                大阪公立大学吹奏楽部は、年2回の演奏会（サマーコンサート、定期演奏会）を中心に活動を行っているほか、学校などへのレッスン、大学や地域のイベントでの依頼演奏など、毎年多くの場面で演奏の機会を頂いております。<br />
-                また、阪神国公立大学吹奏楽連盟に所属している周辺の国公立大学の吹奏楽団や、当部OB・OGの方々とも吹奏楽を通じた交流を行っております。<br />
-                これらの当部の活動に対し、絶えずご協力やご支援を頂戴している全ての方々に心より御礼申し上げます。今後とも大阪公立大学吹奏楽部に対するご支援とご協力をお願い申し上げます。<br />
-            </p>
-        </div>
+  <div class="greeting">
+    <HeroImage fileName="greeting_header.png" title="ごあいさつ" />
+    <div class="large-wrapper">
+      <div class="wrapper">
+        <h1>Osaka Metropolitan University<br />Wind Orchestra</h1>
+        <h2>大阪公立大学吹奏楽部</h2>
+        <p>
+          本日も大阪公立大学吹奏楽部のホームページをご覧いただき、誠にありがとうございます。
+          大阪公立大学吹奏楽部は、年2回の演奏会（サマーコンサート、定期演奏会）を中心に活動を行っているほか、学校などへのレッスン、大学や地域のイベントでの依頼演奏など、毎年多くの場面で演奏の機会を頂いております。<br />
+          また、阪神国公立大学吹奏楽連盟に所属している周辺の国公立大学の吹奏楽団や、当部OB・OGの方々とも吹奏楽を通じた交流を行っております。<br />
+          これらの当部の活動に対し、絶えずご協力やご支援を頂戴している全ての方々に心より御礼申し上げます。今後とも大阪公立大学吹奏楽部に対するご支援とご協力をお願い申し上げます。<br />
+        </p>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -32,27 +28,7 @@ export default {
 
 <style scoped>
 .greeting {
-    min-height: 80vh;
-    position: relative;
-}
-.image_header {
-    margin-right: calc(50% - 50vw);
-    margin-left: calc(50% - 50vw);
-}
-.image_header img {
-    display: block;
-    width: 100%;
-    height: auto;
-    object-fit: cover;
-}
-.contents {
-    margin: 5% auto;
-    width: 83%;
-    max-width: 900px;
-    font-size: 0.9rem;
-}
-.txt_1 {
-    text-align: justify;
-    text-justify: inter-ideograph;
+  min-height: 80vh;
+  position: relative;
 }
 </style>
