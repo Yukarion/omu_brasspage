052af30f498a14e6a1a6742edd694a6db55a9140
Melops <95263124+Melonps@users.noreply.github.com>
2024-01-28 22:14:08 +0900

    [FEAT] Archiveへのルーチングを削除 #62

--- src/router/index.js
+++ src/router/index.js
@@ -2,57 +2,52 @@ import { createRouter, createWebHistory } from "vue-router";
 import Top from "../views/Top.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Top",
-        component: Top,
-    },
-    {
-        path: "/about",
-        name: "About",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ "../views/About.vue"),
-    },
-    {
-        path: "/greeting",
-        name: "Greeting",
-        component: () => import("../views/Greeting.vue"),
-    },
-    {
-        path: "/news",
-        name: "News",
-        component: () => import("../views/News.vue"),
-    },
-    {
-        path: "/advertise",
-        name: "Advertise",
-        component: () => import("../views/Advertise.vue"),
-    },
-    {
-        path: "/request",
-        name: "Request",
-        component: () => import("../views/Request.vue"),
-    },
-    {
-        path: "/archive",
-        name: "Archive",
-        component: () => import("../views/Archive.vue"),
-    },
-    {
-        path: "/portal",
-        name: "Portal",
-        component: () => import("../views/Portal.vue"),
-    },
+  {
+    path: "/",
+    name: "Top",
+    component: Top,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/greeting",
+    name: "Greeting",
+    component: () => import("../views/Greeting.vue"),
+  },
+  {
+    path: "/news",
+    name: "News",
+    component: () => import("../views/News.vue"),
+  },
+  {
+    path: "/advertise",
+    name: "Advertise",
+    component: () => import("../views/Advertise.vue"),
+  },
+  {
+    path: "/request",
+    name: "Request",
+    component: () => import("../views/Request.vue"),
+  },
+  {
+    path: "/portal",
+    name: "Portal",
+    component: () => import("../views/Portal.vue"),
+  },
 ];
 
 const base = "/"; // ここにベースURLを直接指定
 
 const router = createRouter({
-    history: createWebHistory(base),
-    routes,
+  history: createWebHistory(base),
+  routes,
 });
 
 export default router;
